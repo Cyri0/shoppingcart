752672d21f7d24facbd86436a607106118e9c23b
Nemes Tamás - Mechwart <nemestamas94@gmail.com>
2024-03-26 17:03:13 +0100

    first commit

---
+++ shopitem/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class ShopItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class OrderItem(models.Model):
+    shopitem = models.ForeignKey(ShopItem, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.shopitem.name} - {self.amount}db "
+
+class Order(models.Model):
+    # TODO User!
+    items = models.ManyToManyField(OrderItem)
+    date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.date
